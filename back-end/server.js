ab7f78daddb3217b98ef50577eeb449e81a89cdd
Nick <nickq4504@gmail.com>
2023-12-26 18:14:01 -0330

    Back end folder and server.js created.

---
+++ back-end/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { google } = require('googleapis');
+const jwt = require('jsonwebtoken')
+const multer = require("multer");
+const mongo = require("mongodb")
+const {MongoClient} = require("mongodb");
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () => {
+    console.log(`Server now listening on ${PORT}`);
+});
